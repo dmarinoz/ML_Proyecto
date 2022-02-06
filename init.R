676a2665d19e67b7c1a2c9fb4210b0afb0207d96
Daniel Mariño <76059405+dmarinoz@users.noreply.github.com>
2022-02-06 14:41:23 -0500

    Update init.R

--- init.R
+++ init.R
@@ -4,11 +4,11 @@
 #
 
 my_packages = c("shiny", "class", "caTools", "caret", "randomForest", "tree", "e1071", "naivebayes", "C50", "VGAM", "neuralnet","DT")
-
+urlPackage <- "https://cran.r-project.org/src/contrib/Archive/randomForest/randomForest_4.6-12.tar.gz"
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
   }
 }
-
+install.packages(urlPackage, repos=NULL, type="source") 
 invisible(sapply(my_packages, install_if_missing))
