144fe94270642347b767e9fd4dbf37321c579246
Daniel Mariño <76059405+dmarinoz@users.noreply.github.com>
2022-02-06 19:24:43 -0500

    Update init.R

--- init.R
+++ init.R
@@ -3,12 +3,12 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shiny", "class", "caTools", "caret", "randomForest", "tree", "e1071", "naivebayes", "C50", "VGAM", "neuralnet","DT")
-urlPackage <- "https://cran.r-project.org/src/contrib/Archive/randomForest/randomForest_4.6-12.tar.gz"
+my_packages = c("shiny")
+
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
   }
 }
-install.packages(urlPackage, repos=NULL, type="source") 
+
 invisible(sapply(my_packages, install_if_missing))
